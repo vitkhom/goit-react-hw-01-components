12d59a03d035fef84e2113cd8ccd21bd9a194865
vitkhom <vitalii.s.khomenko@gmail.com>
2020-06-02 17:19:24 +0300

    Updates

---
+++ precache-manifest.f9f1b56629e67bd3418a91d8af13c51b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95d33bada7e106b708ef7f9d43d00d9",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "77f88cbba96352bed763",
+    "url": "/goit-react-hw-01-components/static/css/main.eeef88b5.chunk.css"
+  },
+  {
+    "revision": "8a850b8c8154c6e63d3b",
+    "url": "/goit-react-hw-01-components/static/js/2.0da0ae04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.0da0ae04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f88cbba96352bed763",
+    "url": "/goit-react-hw-01-components/static/js/main.5d05fd8b.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
